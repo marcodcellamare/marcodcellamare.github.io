a71e87b59d4ffb283db97e8cf241219d4b291463
Marco D. Cellamare <marco.cellamare@yahoo.it>
2025-06-06 14:52:57 +0200

    C64 loaded

--- src/app/misc/loader/index.tsx
+++ src/app/misc/loader/index.tsx
@@ -1,56 +1,80 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useResize } from '!/contexts/resize';
+import { useSettings } from '!/contexts/settings';
 
 import { IntervalType } from '!/types/misc';
+import { THEMES, ThemeType } from '!/types/config.const';
+import classNames from 'classnames';
 
 type StripType = {
-	h: number;
-	c: string;
+	height: number;
+	color: ThemeType;
 };
 
 const Loader = () => {
 	const { height } = useResize();
+	const { isNavOpened } = useSettings();
 
 	const [strips, setStrips] = useState<StripType[]>([]);
 
 	const intervalRef = useRef<IntervalType>(null);
 
-	const max = 100;
-	const min = 50;
+	const max = 50;
+	const min = 5;
+
+	const cleanup = () => {
+		if (intervalRef.current !== null) {
+			clearInterval(intervalRef.current);
+			intervalRef.current = null;
+		}
+	};
 
 	const stripHeight = () => Math.floor(Math.random() * (max - min + 1) + min);
 
-	/*
 	const generate = useCallback(() => {
 		const strips: StripType[] = [];
+		let prevColor: ThemeType | null = null;
 
 		do {
-			//strips.push(stripHeight());
-		} while (strips.reduce((sum, strip) => sum + strip.h, 0) < height);
+			const height = stripHeight();
+			let color: ThemeType;
 
-		/*
-		console.log(
-			strips,
-			strips.reduce((a, h) => a + h, 0),
-			height
-		);
-		* /
-		//setStrips;
+			do {
+				color = THEMES[Math.floor(Math.random() * THEMES.length)];
+			} while (color === prevColor);
+
+			strips.push({
+				height,
+				color,
+			});
+			prevColor = color;
+		} while (strips.reduce((sum, strip) => sum + strip.height, 0) < height);
+
+		setStrips(strips);
 	}, [height]);
 
 	useEffect(() => {
-		intervalRef.current = setInterval(generate, 300);
+		cleanup();
 		generate();
+
+		intervalRef.current = setInterval(generate, 100);
+
+		return () => cleanup();
 	}, [generate]);
-	*/
 
 	return (
-		<div className='absolute top-0 bottom-0 left-0 right-1/2 bg-white/20 border overflow-hidden'>
+		<div
+			className={classNames([
+				'absolute top-0 bottom-0 left-0 overflow-hidden mix-blend-difference transition-[width] duration-500 delay-300',
+				!isNavOpened ? 'w-[0.25rem]' : 'w-full',
+			])}>
 			{strips.map((strip, k) => (
 				<div
 					key={k}
-					className='border'
-					style={{ height: strip.h }}
+					style={{
+						height: strip.height,
+						backgroundColor: `var(--color-palette-${strip.color})`,
+					}}
 				/>
 			))}
 		</div>
